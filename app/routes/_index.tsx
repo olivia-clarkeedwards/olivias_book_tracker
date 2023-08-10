9ffc8e765f10a3ccc383d971d0515cac4fb10113
Olivia <olivia@chameleoncreator.com>
2023-08-10 17:01:58 +1200

    create form to add book

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,12 @@
-import type { V2_MetaFunction } from "@remix-run/node";
-import { Link, Form } from "@remix-run/react";
-import { SearchField } from "~/components/searchField";
+import { json, type ActionArgs, type V2_MetaFunction } from "@remix-run/node";
+import { useFetcher } from "@remix-run/react";
+import { useEffect, useState } from "react";
+// import { Link, Form } from "@remix-run/react";
+// import { SearchField } from "~/components/searchField";
 
 // import { useOptionalUser } from "~/utils";
 
-const PROGRESS_STATES = ["toread", "reading", "read"];
+// const PROGRESS_STATES = ["toread", "reading", "read"];
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -13,21 +15,99 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+type Book = { title: string; author: string; coverURL: string };
+
 export default function Index() {
   // const user = useOptionalUser();
+  const fetcher = useFetcher();
+  const { Form } = fetcher;
+  const [currentBook, setCurrentBook] = useState<Book>({} as Book);
+
+  useEffect(() => {
+    if (fetcher.data) {
+      setCurrentBook(fetcher.data);
+    }
+  });
   return (
     <main className="h1 relative h-screen min-h-screen bg-red-200 sm:flex sm:items-center sm:justify-center">
       <h1>BookTracker</h1>
-      <Form>
-        <SearchField inputName="search book" placeholder="search book" />
+      <Form method="post" action=".">
+        <input hidden name="__action" value="add-book" />
+        {/* <SearchField inputName="search book" placeholder="search book" /> */}
+        <label htmlFor="title">Title</label>
+        <input
+          name="title"
+          className="w-full rounded-full px-2 py-1 text-center text-lg"
+        />
+        <br />
+        <label htmlFor="author">Author</label>
+        <input
+          name="author"
+          className="w-full rounded-full px-2 py-1 text-center text-lg"
+        />
+        <br />
+        <label htmlFor="cover">Cover image URL</label>
+        <input
+          name="cover"
+          className="w-full rounded-full px-2 py-1 text-center text-lg"
+        />
+        <br />
+        <button
+          className="w-full rounded-full bg-blue-500 px-4 py-2 text-white hover:bg-blue-400 focus:bg-blue-300"
+          type="submit"
+        >
+          Add book to library
+        </button>
       </Form>
 
-      {PROGRESS_STATES.map((state: string) => (
+      <h2>My Library</h2>
+      <ul>
+        {currentBook && (
+          <>
+            <li>{`${currentBook?.title} by ${currentBook.author}`}</li>
+            <img
+              src={currentBook.coverURL}
+              alt={`${currentBook.title} cover`}
+              className="h-20"
+            />
+          </>
+        )}
+        <li>Book2</li>
+        <li>Book3</li>
+      </ul>
+
+      {/* {PROGRESS_STATES.map((state: string) => (
         <div className="card" key={state}>
           <h2>{state.toUpperCase()}</h2>
           <Link to={`/books/${state}`}>View all</Link>
         </div>
-      ))}
+      ))} */}
     </main>
   );
 }
+
+export const action = async (args: ActionArgs) => {
+  const { request } = args;
+  const data = await request.formData();
+
+  const __action = data.get("__action");
+
+  switch (__action) {
+    case "add-book": {
+      return actionAddBook(args, data);
+    }
+  }
+};
+
+const actionAddBook = async (
+  { request, context, params }: ActionArgs,
+  data: FormData
+) => {
+  //get user id?
+
+  const bookTitle = data.get("title");
+  const author = data.get("author");
+  const coverURL = data.get("cover");
+
+  return json({ title: bookTitle, author, coverURL });
+};
