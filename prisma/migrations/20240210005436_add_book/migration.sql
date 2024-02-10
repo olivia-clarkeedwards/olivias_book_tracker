77650718428d2e08229820333aacec184bd8c2ad
Olivia <olivia@chameleoncreator.com>
2024-02-10 15:43:19 +1300

    update db

---
+++ prisma/migrations/20240210005436_add_book/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Note` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Note";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "genre" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Book_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
